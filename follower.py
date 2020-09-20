4e79e4e4f5dd295b7234ae4463d27a57b924c745
Oleg <prytkov.2213@gmail.com>
2020-09-21 02:05:48 +0300

    2:05

--- follower.py
+++ follower.py
@@ -23,17 +23,17 @@ mass = []
 while True:
     while True:
         try:
-            status = get_status(287286283)
+            status = get_status(472177450)
             if status == 1:
                 print('Online')
                 hours = datetime.now().hour
                 minutes = datetime.now().minute
                 mass.append([hours, minutes])
-                sleep(15)
+                sleep(30)
             else:
                 if len(mass) == 0:
                     print('Offline')
-                    sleep(15)
+                    sleep(30)
                 else:
                     if mass[-1][0] == 0 and mass[0][0] == 23:
                         mass[-1][0] = 24
@@ -47,15 +47,16 @@ while True:
                         mass[-1][0] = 26
                     if mass[-1][0] == 2 and mass[0][0] == 22:
                         mass[-1][0] = 26
-                    sess = ((mass[-1][0] * 60 + mass[-1][1]) -
-                            (mass[0][0] * 60 + mass[0][1])) - 5
+                    sess = ((mass[-1][0] * 60 + mass[-1][1]) -(mass[0][0] * 60 + mass[0][1])) - 5
+                    if sess < 0: sess = 0
                     msg = f'Вход - {mass[0][0]}:{mass[0][1]}\nВыход - {mass[-1][0]}:{mass[-1][1]}\nПродолжительность - {sess} минут(ы)'
                     send_msg(msg)
                     print(f'{datetime.now().date()} - Запись завершена')
                     mass.clear()
+                    sleep(30)
         except:
             print('Problem')
-            sleep(15)
+            sleep(30)
 
 # 287286283
 # 472177450
